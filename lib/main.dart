8526f2af058088f9e465248e8f64b8f0e65a786b
Dennis Bozzi <dennisbozzii@gmail.com>
2023-08-22 23:57:02 -0300

    Instalando Firebase Instalei o Firebase porém ele não tem suporte a desktop, então vou instalar o MySQL. Espero conseguir sincronizar todos os dados do MySQL em nuvem para que de alguma forma seja possível ver os dados pelo dispositivo móvel também.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /*await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );*/
+
   runApp(const MyApp());
 }
 
@@ -14,12 +22,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark(useMaterial3: false),
       home: const HomeScreen(),
-      localizationsDelegates: [
+      localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: [
+      supportedLocales: const [
         Locale('pt', 'BR'),
       ],
     );
